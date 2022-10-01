a57d6e2e4106ac661442b9e462ded24dbe220d18
Sultan Alsawaf <sultan@kerneltoast.com>
2022-10-01 21:25:21 +0700

    cpufreq: Don't let userspace set max freq
    Now, Kill userspace changing cpufreq at max freq.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
    Signed-off-by: nerd.zenss_ <nerd.zensdev@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -727,6 +727,9 @@ static ssize_t store_##file_name					\
 		(task_is_booster(current)))				\
 		return count;						\
 									\
+	if (&policy->object == &policy->max)				\
+		return count;						\
+									\
 	memcpy(&new_policy, policy, sizeof(*policy));			\
 	new_policy.min = policy->user_policy.min;			\
 	new_policy.max = policy->user_policy.max;			\
